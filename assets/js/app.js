e6d31b0fa9048d40ab53f9f26cac36db2ebe8051
TomyCaballero <tomyC80@gmail.com>
2023-01-20 22:21:30 -0600

    Se modifico el ejercicio para llegar a su solución

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('.name');
+const b = document.querySelector('.blog');
+const l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  async function displayUser(username) {
+  n.textContent = 'cargando...';
+  const response = await fetch(baseEndpoint)
+  .then (response => response.json())
+  .then (data => console.log(data));
+  n.textContent = '${data.name}';
+  b.textContent = '${data.blog}';
+  l.textContent = '${data.location}';
 }
 
 function handleError(err) {
